fb883d6f03cb0e01d5b7f3c57df02a44c0a09ac9
Liudmy-la <lyouda.malko@gmail.com>
2024-01-16 20:53:11 +0200

    have code cleaned and fixed

--- index.js
+++ index.js
@@ -14,83 +14,89 @@ const elementsContainer = document.querySelector('.elements');
 
 //---------------------------------
 
-// function pullBlock(event) {
-//     const newElement = createNew(event);
+function pullBlock(event) {
+	const newElement = createNew(event);
 
-//     document.addEventListener(
-// 		"mousemove", 
-// 		(moveEvent) => moveBlock(moveEvent, newElement)
-// 	);
+    const shiftX = event.clientX - newElement.getBoundingClientRect().left;
+    const shiftY = event.clientY - newElement.getBoundingClientRect().top;
 
-//     newElement.addEventListener(
-// 		"mouseup", 
-// 		() => quitBlock(newElement)
-// 	);
-// }
+	const moveNewElement = (moveEvent) => moveBlock(moveEvent, newElement, shiftX, shiftY)
 
-// //---------------------------------
+    document.addEventListener(
+		"mousemove", 
+		moveNewElement
+	);
 
-// function createNew(event) {
-//     const newElement = document.createElement('div');
-//     newElement.classList.add('created');
-//     newElement.textContent = `New ${event.target.innerText}`;
+    newElement.addEventListener(
+		"mouseup", 
+		() => {			
+			if (parseFloat(newElement.style.top) - newElement.getBoundingClientRect().top < 70) {
+				newElement.style.top = newElement.getBoundingClientRect().top + 70 + 'px';
+			}
 
-//     newElement.style.position = 'absolute';
-//     newElement.style.left = event.target.getBoundingClientRect().left + 'px';
-//     newElement.style.top = event.target.getBoundingClientRect().top + 'px';
+			document.removeEventListener(
+				"mousemove", 
+				moveNewElement
+			);
+		}
+	);
+}
+//---------------------------------
 
-//     elementsContainer.appendChild(newElement);
-//     return newElement;
-// }
+function createNew(event) {
+    const newElement = document.createElement('div');
+		newElement.classList.add('created');
+		newElement.textContent = `New ${event.target.innerText}`;
 
-// //---------------------------------
+		newElement.style.position = 'absolute';
+		newElement.style.left = event.target.getBoundingClientRect().left + 'px';
+		newElement.style.top = event.target.getBoundingClientRect().top + 'px';
 
-// function moveBlock (event, element) {
-//     const shiftX = event.clientX - element.getBoundingClientRect().left;
-//     const shiftY = event.clientY - element.getBoundingClientRect().top;
-//     element.style.left = event.clientX - shiftX + 'px';
-//     element.style.top = event.clientY - shiftY + 'px';
-// }
+    elementsContainer.appendChild(newElement);    
+    return newElement;
+}
+//---------------------------------
 
-// //---------------------------------
+function moveBlock (event, element, shiftX, shiftY) {
+    element.style.left = event.clientX - shiftX + 'px';
+    element.style.top = event.clientY - shiftY + 'px';
+}
+//---------------------------------
 
 // function quitBlock (element) {
 //     if (parseFloat(element.style.top) - element.getBoundingClientRect().top < 70) {
-//         element.style.top = element.getBoundingClientRect().top + 70 + 'px';
-//     }
-
-//     document.removeEventListener(
-// 		"mousemove", 
-// 		(moveEvent) => moveBlock(moveEvent, element)
-// 	);
+// 		element.style.top = element.getBoundingClientRect().top + 70 + 'px';
+// 	}
 // }
 
 
-function pullBlock(event) {
-	const newElement = document.createElement('div');
-		newElement.classList.add('created');
-		newElement.textContent = `New ${event.target.innerText}`;
-		newElement.style.position = 'absolute';
+// function pullBlock(event) {
+// 	debugger
 
-		newElement.style.left = event.target.getBoundingClientRect().left + `px`
-		newElement.style.top = event.target.getBoundingClientRect().top + `px`
- 	elementsContainer.appendChild(newElement);
+// 	const newElement = document.createElement('div');
+// 		newElement.classList.add('created');
+// 		newElement.textContent = `New ${event.target.innerText}`;
+// 		newElement.style.position = 'absolute';
 
-		let shiftX = event.clientX - newElement.getBoundingClientRect().left;
-		let shiftY = event.clientY - newElement.getBoundingClientRect().top;
+// 		newElement.style.left = event.target.getBoundingClientRect().left + `px`
+// 		newElement.style.top = event.target.getBoundingClientRect().top + `px`
+//  	elementsContainer.appendChild(newElement);
+
+// 		let shiftX = event.clientX - newElement.getBoundingClientRect().left;
+// 		let shiftY = event.clientY - newElement.getBoundingClientRect().top;
 	
- 	document.addEventListener("mousemove", moveBlock);
-	function moveBlock (event) {
-		newElement.style.left = event.clientX - shiftX + `px`
-		newElement.style.top = event.clientY - shiftY + `px`
-	}
+//  	document.addEventListener("mousemove", moveBlock);
+// 	function moveBlock (event) {
+// 		newElement.style.left = event.clientX - shiftX + `px`
+// 		newElement.style.top = event.clientY - shiftY + `px`
+// 	}
 
-	newElement.addEventListener("mouseup", quitBlock);
-	function quitBlock () {
-		if (parseFloat(newElement.style.top) - event.target.getBoundingClientRect().top < 70) {
-				newElement.style.top = event.target.getBoundingClientRect().top + 70 + `px`
-		}
+// 	newElement.addEventListener("mouseup", quitBlock);
+// 	function quitBlock () {
+// 		if (parseFloat(newElement.style.top) - event.target.getBoundingClientRect().top < 70) {
+// 				newElement.style.top = event.target.getBoundingClientRect().top + 70 + `px`
+// 		}
 
-		document.removeEventListener("mousemove", moveBlock);
-	}
-}
+// 		document.removeEventListener("mousemove", moveBlock);
+// 	}
+// }
